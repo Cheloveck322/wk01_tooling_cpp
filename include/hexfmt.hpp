8f8c63938b14f49be1f60510acd71a08f7ae2ade
Cheloveck322 <vasyagri2005@gmail.com>
2025-10-04 00:39:12 +0300

    adding hexfmt, main

--- include/hexfmt.hpp
+++ include/hexfmt.hpp
@@ -1,6 +1,11 @@
 #ifndef HEXFMT_HPP
 #define HEXFMT_HPP
 
+#include <cstddef>
+#include <cstdint>
+#include <span>
+#include <string>
 
+std::string format_line(std::uint64_t offset, std::span<const std::byte> data, std::size_t width);
 
 #endif
--- src/args.cpp
+++ src/args.cpp
@@ -1,6 +1,59 @@
 #include "../include/args.hpp"
+#include <iostream>
 
 Options parse_cli(int argc, char** argv)
 {
-    
+    Options opt;
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string option{ argv[i] };
+        if (option == "-h" || option == "--help")
+        {
+            print_help(argv[1], std::cout);
+            std::exit(0);
+        }
+        else if (option == "-w" || option == "--width")
+        {
+            size_t new_width{ static_cast<size_t>(std::stoull(argv[++i])) };
+            if (!(new_width == 8 || new_width == 16 || new_width == 32)) 
+            {
+                std::cerr << "Width must be 8, 16, or 32\n"; std::exit(1);
+            }
+            else 
+            {
+                opt.width = new_width;
+            }
+        }
+        else if (option == "-n" || option == "--limit")
+        {
+            opt.limit = static_cast<size_t>(std::stoull(argv[++i]));
+        }
+        else if (option == "-o" || option == "--offset")
+        {
+            opt.offset = static_cast<size_t>(std::stoull(argv[++i]));
+        }
+        else if (option.rfind("-", 0) != std::string::npos)
+        {
+            std::cerr << "Unknown option: " << option << std::endl;
+            print_help(argv[1], std::cout);
+        }
+        else 
+        {
+            opt.file = option;
+        }
+    }
+
+    return opt;
+}
+
+void print_help(std::string_view prog, std::ostream& out)
+{
+    out << 
+        "Usage: " << prog << "\thexdump-lite [options] <file>...\n"
+        "Display file contents in hexadcimal or ascii.\n"
+        "Opitons:\n"
+        "  -w, --width <8|16|32>   bytes per line (default 16)\n"
+        "  -n, --limit <N>         limit total bytes\n"
+        "  -o, --offset <N>        start offset\n"
+        "  -h, --help              show this help\n";
 }
